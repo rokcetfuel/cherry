27ab5722fc3c833a1a8abcc28321b7a5e43a590e
Kamila Kot <shallovvv@gmail.com>
2021-05-29 15:46:19 +0200

    learn mode finish

--- src/components/learn/LearnFinished.js
+++ src/components/learn/LearnFinished.js
@@ -4,14 +4,27 @@ export default function LearnFinished(props) {
   const { count, again, done } = props
 
   return (
-    <div className='c-learn-finished'>
+    <>
       <div className='c-main'>
-        good job! 
-        you revised all {count} phrases.
-
-        <button onClick={again}>go again</button>
-        <button onClick={done}>done</button>
+        <div className='c-learn-finish__top'>
+          <span className='c-btn'>
+            good job!
+          </span>
+        </div>
+        <div className='c-learn-finish__bottom'>
+          <div className='c-learn-finish__text'>
+            You revised all <span>{count}</span> phrases.
+          </div>
+          <div className='c-learn-finish__actions'>
+            <button className='c-learn-finish__actions-btn' onClick={again}>
+              go again
+            </button>  
+            <button className='c-learn-finish__actions-btn' onClick={done}>
+              done
+            </button>  
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
